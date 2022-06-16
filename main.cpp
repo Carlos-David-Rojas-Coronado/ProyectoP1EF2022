166ba8d0dcc750eb9917a38410e49b31a64d5264
Carlos-David-Rojas-Coronado <crojasc3@miumg.edu.gt>
2022-06-16 12:16:02 -0600

    menu inicial

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
 
-using namespace std;
+#define MAX 80
+#define ARCHIVO_USUARIOS "usuarios.dat"
+#define TECLA_ENTER 13
+#define TECLA_BACKSPACE 8
+#define LONGITUD 5
+#define MAX_INTENTOS 3
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+/* Estructura de cuentas de usuario */
+struct usuario {
+	char nombre[MAX];
+	char password[MAX];
+};
+
+typedef struct usuario Usuario;
+
+/* Funciones de menú */
+void menuInicial();
+void menuListarUsuarios();
+void menuRegistrarUsuario();
+void menuIniciarSesion();
+void menuSistema();
+
+/* Funciones que manipulan el archivo de usuarios */
+char insertarUsuario(Usuario usuario);
+char existeUsuario(char nombreUsuario[], Usuario *usuario);
+Usuario *obtenerUsuarios(int *);
+char logear(char nombreUsuario[], char password[]);
+
+int leerLinea(char *cad, int n);
+void leerClave(char *password);
+
+char linea[MAX];
+int main() {
+	menuInicial();
+	return 0;
 }
