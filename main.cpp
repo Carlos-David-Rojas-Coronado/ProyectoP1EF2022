6539d8ad51e53b5f8121c10a1f11821b180eafb4
Carlos-David-Rojas-Coronado <crojasc3@miumg.edu.gt>
2022-06-16 12:20:46 -0600

    menu general

--- main.cpp
+++ main.cpp
@@ -39,3 +39,149 @@ int main() {
 	menuInicial();
 	return 0;
 }
+void menuInicial() {
+	char repite = 1;
+	int opcion = -1;
+
+	do {
+		system("cls");
+		printf("\n\t\t\tMENU INICIAL\n");
+		printf("\t\t\t============\n");
+		printf("\n\t\t[1]. Ver usuarios registrados\n");
+		printf("\t\t[2]. Registrar usuario nuevo\n");
+		printf("\t\t[3]. Ingresar al sistema\n");
+		printf("\t\t[0]. Salir\n");
+		printf("\n\t\tIngrese su opcion: [ ]\b\b");
+		leerLinea(linea, MAX);
+		sscanf(linea, "%d", &opcion);
+
+		switch (opcion) {
+			case 1:
+				menuListarUsuarios();
+				break;
+
+			case 2:
+				menuRegistrarUsuario();
+				break;
+
+			case 3:
+				menuIniciarSesion();
+				break;
+
+			case 0:
+				repite = 0;
+				break;
+		}
+
+	} while (repite == 1);
+}
+
+void menuRegistrarUsuario() {
+	Usuario usuario;
+	char nombreUsuario[MAX];
+	char respuesta[MAX];
+	char repite = 1;
+
+	do {
+		system("cls");
+		printf("\n\t\t\tREGISTRAR USUARIO\n");
+		printf("\t\t\t=================\n");
+		printf("\n\tIngrese nombre de usuario: ");
+		leerLinea(linea, MAX);
+		sscanf(linea, "%s", nombreUsuario);
+
+		/* Se verifica que el nombre de usuario no exista */
+		if (!existeUsuario(nombreUsuario, &usuario)) {
+			strcpy(usuario.nombre, nombreUsuario);
+
+			printf("\tIngrese la clave: ");
+			leerLinea(usuario.password, MAX);
+
+			/* Se inserta el usuario en el archivo de usuarios */
+			if (insertarUsuario(usuario)) {
+				printf("\n\tEl usuario fue registrado satisfactoriamente!\n");
+
+			} else {
+				printf("\n\tOcurrio un error al registrar el usuario\n");
+				printf("\nInténtelo mas tarde\n");
+			}
+		} else {
+			printf("\n\tEl usuario \"%s\" ya ha sido registrado previamente\n", usuario.nombre);
+			printf("\tNo puede registrar dos usuarios con el mismo nombre de usuario.\n");
+		}
+
+		printf("\n\tDesea seguir registrando usuarios? [S/N]: ");
+		leerLinea(respuesta, MAX);
+
+		if (!(strcmp(respuesta, "S") == 0 || strcmp(respuesta, "s") == 0)) {
+			repite = 0;
+		}
+
+	} while (repite == 1);
+}
+
+void menuListarUsuarios() {
+	int numeroUsuarios = 0;
+	Usuario *usuarios;
+	int i;
+
+	system("cls");
+	usuarios = obtenerUsuarios(&numeroUsuarios); /* Retorna un vector dinámico de usuarios */
+
+	if (numeroUsuarios == 0) {
+		printf("\n\tNo existe ningun usuario registrado!\n");
+
+	} else {
+		printf("\n\t\t    ==> LISTADO DE USUARIOS REGISTRADOS <==\n");
+		printf(" ------------------------------------------------------------------------------\n");
+		printf("%10s%25s%25s\n", "#", "NOMBRE", "PASSWORD");
+		printf(" ------------------------------------------------------------------------------\n");
+
+		/* Se recorre el vector dinámico de productos */
+		for (i = 0; i < numeroUsuarios; i++) {
+			printf("%10d%25s%25s\n", (i + 1), usuarios[i].nombre, usuarios[i].password);
+		}
+		printf(" ------------------------------------------------------------------------------\n");
+	}
+
+	// Se libera la memoria asignada al vector dinámico
+	free(usuarios);
+	usuarios = NULL;
+	getchar();
+}
+
+void menuIniciarSesion() {
+	char nombreUsuario[MAX];
+	char password[MAX];
+	int intento = 0;
+	int loginExitoso = 0;
+
+	do {
+		system("cls");
+		printf("\n\t\t\tINGRESAR AL SISTEMA\n");
+		printf("\t\t\t===================\n");
+
+		printf("\n\t\tUSUARIO: ");
+		leerLinea(linea, MAX);
+		sscanf(linea, "%s", nombreUsuario);
+
+		printf("\t\tCLAVE: ");
+		leerClave(password);
+
+		if (logear(nombreUsuario, password)) {
+			loginExitoso = 1;
+		} else {
+			printf("\n\n\t\tUsuario y/o password incorrectos");
+			intento++;
+			getchar();
+		}
+	} while (intento < MAX_INTENTOS && loginExitoso == 0);
+
+	if (loginExitoso == 1) {
+		menuSistema();
+
+	} else {
+		printf("\n\tHa sobrepasado el numero maximo de intentos permitidos\n");
+		getchar();
+	}
+}
